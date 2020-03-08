96e245c72e6251fd94c77b8789126f6fdd41f6cc
Romane G <romane71193@gmail.com>
2020-03-08 12:27:14 -0400

    Updates

---
+++ precache-manifest.9bfe187c5dc06f1ccbeed0913e22818f.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = [
+  {
+    "revision": "f39460baac0708e01551",
+    "url": "/su-ui-rg/static/css/main.579c0470.chunk.css"
+  },
+  {
+    "revision": "f39460baac0708e01551",
+    "url": "/su-ui-rg/static/js/main.f39460ba.chunk.js"
+  },
+  {
+    "revision": "7b4cbc1190bc819fca1d",
+    "url": "/su-ui-rg/static/css/1.0ebe1689.chunk.css"
+  },
+  {
+    "revision": "7b4cbc1190bc819fca1d",
+    "url": "/su-ui-rg/static/js/1.7b4cbc11.chunk.js"
+  },
+  {
+    "revision": "053155204358bb55fc8b",
+    "url": "/su-ui-rg/static/js/runtime~main.05315520.js"
+  },
+  {
+    "revision": "647941cb8b4b782b8de1bb70bb628e03",
+    "url": "/su-ui-rg/static/media/logo-circular.647941cb.svg"
+  },
+  {
+    "revision": "dd67e7aacc49b7ada6eb1928df06d16e",
+    "url": "/su-ui-rg/static/media/basic-plan.dd67e7aa.svg"
+  },
+  {
+    "revision": "b809d4e84d93a14b1e1aeb24a3701d31",
+    "url": "/su-ui-rg/static/media/logo.b809d4e8.png"
+  },
+  {
+    "revision": "d6e8b8ab93474081a8d73907ad00680e",
+    "url": "/su-ui-rg/static/media/prefeered-plan.d6e8b8ab.svg"
+  },
+  {
+    "revision": "6f2c199b52bad1be7247c455e7b687fa",
+    "url": "/su-ui-rg/static/media/profile-avatar-6.6f2c199b.png"
+  },
+  {
+    "revision": "cabf16162b1381f75a9141a255736e2c",
+    "url": "/su-ui-rg/static/media/intro-image-1@2x.cabf1616.png"
+  },
+  {
+    "revision": "8dddfebe623c715e9f17b129237152dd",
+    "url": "/su-ui-rg/static/media/intro-image-mobile-1@2x.8dddfebe.png"
+  },
+  {
+    "revision": "1c3a4fcdd1c4abcf3c58a9e5bd4b60f0",
+    "url": "/su-ui-rg/static/media/repair.1c3a4fcd.png"
+  },
+  {
+    "revision": "813143f6fc00ecaf8e1157f2dd42c892",
+    "url": "/su-ui-rg/static/media/estate-image-1.813143f6.png"
+  },
+  {
+    "revision": "19fe4ab7de482fac53f544a175fcc43f",
+    "url": "/su-ui-rg/static/media/estate-image-2.19fe4ab7.png"
+  },
+  {
+    "revision": "31ee83eb5a7a63ebd8dc9975f01ee5aa",
+    "url": "/su-ui-rg/static/media/estate-image-3.31ee83eb.png"
+  },
+  {
+    "revision": "c41d79730dbe5b92355f3b37959a2c88",
+    "url": "/su-ui-rg/static/media/estate-image-4.c41d7973.png"
+  },
+  {
+    "revision": "6247927dbf36cc1a68f3a90a94350c8b",
+    "url": "/su-ui-rg/static/media/estate-image-5.6247927d.png"
+  },
+  {
+    "revision": "0ce39ff32bd7232d4f09e24f3b9bc95e",
+    "url": "/su-ui-rg/static/media/estate-image-6.0ce39ff3.png"
+  },
+  {
+    "revision": "91237500eb8c3bc796b339cffba90057",
+    "url": "/su-ui-rg/static/media/estate-image-7.91237500.png"
+  },
+  {
+    "revision": "0b52e92bf41f45509640bb4de0396a27",
+    "url": "/su-ui-rg/static/media/estate-image-8.0b52e92b.png"
+  },
+  {
+    "revision": "dd306011f996a57944e945b5fcacb371",
+    "url": "/su-ui-rg/static/media/estate-image-9.dd306011.png"
+  },
+  {
+    "revision": "ae75d272e60cac57bf0727e5b5cf42e7",
+    "url": "/su-ui-rg/static/media/estate-image-10.ae75d272.png"
+  },
+  {
+    "revision": "827eae3760410f9df7984bf209663096",
+    "url": "/su-ui-rg/static/media/estate-image-11.827eae37.png"
+  },
+  {
+    "revision": "d140127f623ae450b816ecba000f4032",
+    "url": "/su-ui-rg/static/media/estate-image-12.d140127f.png"
+  },
+  {
+    "revision": "e2169eb6851c26198ad0eb44433f209f",
+    "url": "/su-ui-rg/static/media/user-avtar-3.e2169eb6.png"
+  },
+  {
+    "revision": "0ff251011cc7865538093a65a2b58e1f",
+    "url": "/su-ui-rg/static/media/user-avtar-4.0ff25101.png"
+  },
+  {
+    "revision": "4e71dfb3d53424fd11e6c78f262ddacc",
+    "url": "/su-ui-rg/static/media/first-mover-plan.4e71dfb3.svg"
+  },
+  {
+    "revision": "332184ee8f13334e3bc4132b28512f3c",
+    "url": "/su-ui-rg/static/media/buyer-profile-1.332184ee.png"
+  },
+  {
+    "revision": "bb83c281d9bdd8cbfc0b0c9110035a39",
+    "url": "/su-ui-rg/static/media/buyer-profile-2.bb83c281.png"
+  },
+  {
+    "revision": "801a01b8dc5d55712171b9a4d4a6dd9b",
+    "url": "/su-ui-rg/static/media/buyer-profile-3.801a01b8.png"
+  },
+  {
+    "revision": "822bc40d2711707cd5c980d804066d78",
+    "url": "/su-ui-rg/static/media/buyer-profile-4.822bc40d.png"
+  },
+  {
+    "revision": "037d3915a63039365cc205fdc3463aaf",
+    "url": "/su-ui-rg/static/media/buyer-profile-5.037d3915.png"
+  },
+  {
+    "revision": "a8933734d00f390badb0ecb174953b75",
+    "url": "/su-ui-rg/static/media/buyer-profile-6.a8933734.png"
+  },
+  {
+    "revision": "1c692e8b7f7a734fa07012fae32dfba7",
+    "url": "/su-ui-rg/static/media/visa_Logo.1c692e8b.png"
+  },
+  {
+    "revision": "c65119df3af6b6e7041d641c18b1b508",
+    "url": "/su-ui-rg/static/media/intro-image-2.c65119df.png"
+  },
+  {
+    "revision": "5bf89f1dcb3366e4254e72690c60f871",
+    "url": "/su-ui-rg/static/media/intro-image-2-1.5bf89f1d.png"
+  },
+  {
+    "revision": "b2f919189da871e228e5c688845bc2e6",
+    "url": "/su-ui-rg/static/media/buyer-image-1.b2f91918.png"
+  },
+  {
+    "revision": "4fbbb2130c739ea14d7832f42d9d88c4",
+    "url": "/su-ui-rg/static/media/buyer-image-2.4fbbb213.png"
+  },
+  {
+    "revision": "b49a67c84a19fffe17f79962ca6db071",
+    "url": "/su-ui-rg/static/media/intro-bg.b49a67c8.png"
+  },
+  {
+    "revision": "26b3f6c159ad919fac41a353a7821ae6",
+    "url": "/su-ui-rg/static/media/footer-user.26b3f6c1.png"
+  },
+  {
+    "revision": "b83c0671c4c9a4c279c0d5c47087397d",
+    "url": "/su-ui-rg/static/media/no-img.b83c0671.jpg"
+  },
+  {
+    "revision": "99a1274f45047caf47ad6cb951a4cea5",
+    "url": "/su-ui-rg/static/media/no-plan.99a1274f.jpg"
+  },
+  {
+    "revision": "4245a4d8b23314752994b4cb8969168e",
+    "url": "/su-ui-rg/static/media/no-video.4245a4d8.jpg"
+  },
+  {
+    "revision": "f80e7697ecc36950151a65bfe274def8",
+    "url": "/su-ui-rg/static/media/how-it-works.f80e7697.png"
+  },
+  {
+    "revision": "ea82ac4dd0d1e5faea35673593483ae9",
+    "url": "/su-ui-rg/static/media/SB-icon.ea82ac4d.eot"
+  },
+  {
+    "revision": "9923aaff3dd58d9943773c3fa1ebffdc",
+    "url": "/su-ui-rg/static/media/SB-icon.9923aaff.woff"
+  },
+  {
+    "revision": "b11bb411012721d6955b9d490cd39263",
+    "url": "/su-ui-rg/static/media/SB-icon.b11bb411.ttf"
+  },
+  {
+    "revision": "a9bb043fabd608c2191e251d5b52aa5c",
+    "url": "/su-ui-rg/static/media/SB-icon.a9bb043f.svg"
+  },
+  {
+    "revision": "3f32c9f6bc5ce4e7ac8c4468f36b8aa3",
+    "url": "/su-ui-rg/index.html"
+  }
+];
